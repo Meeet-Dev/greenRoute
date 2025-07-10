200d3965abefa5d3e29fbaaa6607482991eb29cc
Meet Jadav <jadavmeet2203@gmail.com>
2025-07-10 09:14:13 +0000

    Initial project setup with React frontend and Express backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('GreenRoute Backend');
+});
+
+app.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
